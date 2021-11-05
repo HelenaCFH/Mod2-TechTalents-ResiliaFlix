015f728ddeb7ac2350f52674e30b2fcd8f203f10
assueria <ellyassueria@gmail.com>
2021-11-05 08:31:24 -0300

    Nome do arquivo html alterado para ser melhor referenciado no deploy

--- Home/js/script.js
+++ Home/js/script.js
@@ -1,15 +1,44 @@
-$(document).ready(function () {
-
-    $('.first-button').on('click', function () {
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
   
-      $('.animated-icon1').toggleClass('open');
-    });
-    $('.second-button').on('click', function () {
+      this.handleClick = this.handleClick.bind(this);
+    }
   
-      $('.animated-icon2').toggleClass('open');
-    });
-    $('.third-button').on('click', function () {
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
   
-      $('.animated-icon3').toggleClass('open');
-    });
-  });
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+  );
+  mobileNavbar.init();
